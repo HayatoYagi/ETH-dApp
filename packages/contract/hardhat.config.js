9cb4a1b61d60ac7ba0d28475a3b34aa85d58df73
Hayato Yagi <hayatoy1013@gmail.com>
2023-12-19 03:40:41 +0900

    Deploy to sepolia test network

--- packages/contract/hardhat.config.js
+++ packages/contract/hardhat.config.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.SEPOLIA_ACCOUNT_PRIVATE_KEY],
+    },
+  },
 };
--- packages/contract/scripts/deploy.js
+++ packages/contract/scripts/deploy.js
@@ -1,12 +1,14 @@
 const main = async () => {
   const [deployer] = await hre.ethers.getSigners();
   const accountBalance = await deployer.getBalance();
+
+  console.log('Deploying contracts with account: ', deployer.address);
+  console.log('Account balance: ', accountBalance.toString());
+
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
   const waveContract = await waveContractFactory.deploy();
   const wavePortal = await waveContract.deployed()
 
-  console.log('Deploying contracts with account: ', deployer.address);
-  console.log('Account balance: ', accountBalance.toString());
   console.log('Contract deployed to: ', wavePortal.address);
   console.log('Contract deployed by: ', deployer.address);
 };
