c0661c5a9cc9370dd820ee81fbbfca8571174177
Hayato Yagi <hayatoy1013@gmail.com>
2023-12-20 00:07:26 +0900

    Connect wallet to app

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,9 +1,55 @@
-import * as React from 'react';
+import React, { useEffect, useState } from "react";
 
-import './App.css';
+import "./App.css";
 
 export default function App() {
   const wave = () => {};
+
+  const [currentAccount, setCurrentAccount] = useState("");
+  useEffect(() => {
+    console.log("currentAccount: ", currentAccount);
+  }, [currentAccount]);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Make sure you have MetaMask!");
+        return;
+      }
+      console.log("We have the ethereum object", ethereum);
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      if (accounts.length > 0) {
+        console.log("Found an authorized accounts: ", accounts);
+        setCurrentAccount(accounts[0]);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected: ", accounts);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
   return (
     <div className="mainContainer">
@@ -27,6 +73,14 @@ export default function App() {
 
         <button className="waveButton" onClick={wave}>
           Wave at Me
+        </button>
+
+        <button
+          className="waveButton"
+          onClick={connectWallet}
+          disabled={currentAccount}
+        >
+          {currentAccount ? "Wallet Connected" : "Connect Wallet"}
         </button>
       </div>
     </div>
